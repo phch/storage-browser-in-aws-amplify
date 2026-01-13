36a1ee9311cefadd2cf0d792dd3a34f76a3b73b9
phch <philip.chen4@gmail.com>
2026-01-13 15:06:05 +0000

    Add auth and secured routing

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,8 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      otpLogin: true // Enable email-based one-time passwords
+    }
   },
 });
