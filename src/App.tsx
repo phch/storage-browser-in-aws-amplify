119395435bf05184cd72a944e77280ad03a61d08
phch <philip.chen4@gmail.com>
2026-01-15 20:47:26 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
+
+import * as React from 'react';
+
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import outputs from '@/amplify_outputs.json';
+import '@cloudscape-design/global-styles/index.css';
+
+Amplify.configure(outputs);
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator.Provider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Authenticator.Provider>
   );
 }
 
